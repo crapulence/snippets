a5fe520652a0f07ca9f78134c9e60ca18ee6217a
crapulence <shanesong@outlook.com>
2019-12-04 09:57:55 +0800

    feat: readFile

---
+++ js/readFile.ts
@@ -0,0 +1,22 @@
+export type ResultType = "dataUrl" | "text" | "file";
+
+export function readFile(file: File, resultType: ResultType) {
+  return new Promise(resolve => {
+    if (resultType === 'file') {
+      resolve();
+      return;
+    }
+
+    const reader = new FileReader();
+    
+    reader.onload = event => {
+      resolve((event.target as FileReader).result);
+    };
+
+    if (resultType === 'dataUrl') {
+      reader.readAsDataURL(file);
+    } else if (resultType === 'text') {
+      reader.readAsText(file);
+    }
+  })
+}
