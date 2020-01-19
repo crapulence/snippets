7e0e6dde305b323bd411b83c4768a0cbfbe81238
crapulence <shanesong@outlook.com>
2020-01-19 14:58:40 +0800

    feat: add some methods

---
+++ js/getHexColor.ts
@@ -0,0 +1,5 @@
+export function getHexColor() {
+  return (
+    '#' + ('00000' + ((Math.random() * 0x1000000) << 0).toString(16)).slice(-6)
+  );
+}
--- js/number.ts
+++ js/number.ts
@@ -1,3 +1,11 @@
 export function isNumber(value: string): boolean {
   return /^\d+(\.\d+)?$/.test(value);
 }
+
+export function average(array: number[]): number {
+  return array.reduce((acc, val) => acc + val, 0) / array.length;
+}
+
+export function factorial(n: number): number {
+  return n <= 1 ? 1 : n * factorial(n - 1);
+}
