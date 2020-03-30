9542dd080fd4c36e123d644a8594af5973065421
crapulence <shanesong@outlook.com>
2020-03-30 15:11:39 +0800

    feat: add download file

---
+++ js/getFileStream.ts
@@ -0,0 +1,19 @@
+type Data = File | Blob | MediaSource
+
+/**
+ * 获取文件流下载
+ * @param data 数据
+ * @param name 名称
+ */
+export function getFileStream(data: Data, name: string): void {
+  if (window.navigator.msSaveOrOpenBlob) {
+    navigator.msSaveBlob(data, name);
+  } else {
+    const link = document.createElement('a');
+    
+    link.href = window.URL.createObjectURL(data);
+    link.download = name;
+    link.click();
+    window.URL.revokeObjectURL(link.href);
+  }
+}
