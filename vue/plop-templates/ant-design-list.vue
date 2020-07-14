7d08e1e3f38ccba68daab61167af71ebb447f9ab
Shane <shanesong@outlook.com>
2020-07-14 16:58:16 +0800

    chroe

--- vue/plop-templates/ant-design-list.vue
+++ vue/plop-templates/ant-design-list.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="standard-search-page">
     <a-card :bordered="false">
-      <a-form-model layout="inline" :model="form">
-        <a-form-model-item label="名称">
-          <a-input v-model="form.name" placeholder="请输入名称" />
-        </a-form-model-item>
-        <a-form-model-item>
-          <a-button type="primary" icon="search" @click="handleSearch"
-            >搜索</a-button
-          >
-          <a-button icon="redo" @click="handleReset">重置</a-button>
-        </a-form-model-item>
+      <a-form-model :model="form" class="form">
+        <a-row :gutter="12">
+          <a-col :xl="8" :xxl="6">
+            <a-form-model-item label="名称">
+              <a-input v-model="form.name" placeholder="请输入名称" />
+            </a-form-model-item>
+          </a-col>
+          <a-col span="24" class="mt">
+            <a-button type="primary" icon="search" @click="handleSearch">搜索</a-button>
+            <a-button icon="redo" @click="handleReset">重置</a-button>
+          </a-col>
+        </a-row>
       </a-form-model>
       <div class="mt">
         <a-button type="primary" icon="plus" @click="handleAdd">新增</a-button>
@@ -25,17 +27,10 @@
         @change="handleTableChange"
       >
         <template slot="action" slot-scope="record">
-          <a-button
-            size="small"
-            icon="edit"
-            type="link"
-            @click="handleEdit(record.id)"
+          <a-button size="small" icon="edit" type="link" @click="handleEdit(record.id)"
             >编辑</a-button
           >
-          <a-popconfirm
-            title="确认是否删除?"
-            @confirm="handleDelete(record.id)"
-          >
+          <a-popconfirm title="确认是否删除?" @confirm="handleDelete(record.id)">
             <a-button size="small" icon="delete" type="link">删除</a-button>
           </a-popconfirm>
         </template>
@@ -46,16 +41,23 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Ref } from 'vue-property-decorator';
+import { cloneDeep } from 'lodash';
 import ModuleForm from './ant-design-form.vue';
 
-const getList = (params: any) => new Promise<any>(resolve => setTimeout(() => {
-  resolve()
-}, 500));
+const getList = (params: any) =>
+  new Promise<any>((resolve) =>
+    setTimeout(() => {
+      resolve();
+    }, 500)
+  );
 
-const remove = (params: any) => new Promise<any>(resolve => setTimeout(() => {
-  resolve()
-}, 500));
+const remove = (params: any) =>
+  new Promise<any>((resolve) =>
+    setTimeout(() => {
+      resolve();
+    }, 500)
+  );
 
 interface IPaginationConfig {
   current: number;
@@ -78,9 +80,7 @@ export default class extends Vue {
     name: '',
   };
 
-  private form: any = {
-    ...this.defaultForm
-  };
+  private form = cloneDeep({}, this.defaultForm);
 
   private columns: any[] = [
     {
@@ -117,6 +117,9 @@ export default class extends Vue {
   };
 
   private loading: boolean = false;
+
+  @Ref('moduleForm')
+  private moduleForm!: any;
 
   public created() {
     this.getList(this.defaultSearch);
@@ -167,16 +170,20 @@ export default class extends Vue {
 
   private handleReset() {
     this.resetPager();
-    this.form = this.defaultForm;
+    this.form = cloneDeep({}, this.defaultForm);
     this.getList(this.defaultSearch);
   }
 
   private handleAdd() {
-    (this.$refs.moduleForm as any).add();
+    this.moduleForm.add();
   }
 
   private handleEdit(id) {
-    (this.$refs.moduleForm as any).edit(id);
+    this.moduleForm.edit(id);
+  }
+
+  private handleDetail(id) {
+    this.moduleForm.detail(id);
   }
 
   private handleDelete(id) {
@@ -199,17 +206,15 @@ export default class extends Vue {
 </script>
 
 <style lang="less" scoped>
-.standard-search-page {
-  & > .ant-form-item {
-    &:last-child {
-      margin-bottom: 0;
-    }
+.form {
+  .ant-form-item:last-child {
+    margin-bottom: 0;
+  }
 
-    .ant-select,
-    .ant-input,
-    .ant-input-number {
-      width: 250px;
-    }
+  .ant-select,
+  .ant-input,
+  .ant-calendar-picker {
+    width: 100%;
   }
 }
 
