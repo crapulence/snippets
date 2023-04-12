3a5be813da605ae6cc821f13ac5c4fa446c717bd
Shane <shanesong@outlook.com>
2023-04-12 11:42:27 +0800

    feat: 新增支持多个顶级树的路径查找方法

--- js/getPathByTree.ts
+++ js/getPathByTree.ts
@@ -18,3 +18,43 @@ export function getPathByTree(tree: any[], fn: Function, path: any[] = []) {
   }
   return [];
 }
+
+/**
+ * 支持多个顶级树的路径搜索
+ * @param trees
+ * @param fn
+ * @param path
+ * @param results
+ * @returns
+ *
+ * @example
+ * const data = [{label: 'a', value: 1, children: [{label: 'a-1', value: 2}]}, {label: 'a', value: 1, children: [{label: 'a-2', value: 2}]}]
+ * const results = getPathByTrees(data, (item: any) => item.label === 'a')
+ * @returns
+ * [
+ *   [ { label: 'a', value: 1, children: [Array] } ],
+ *   [ { label: 'a', value: 1, children: [Array] } ]
+ * ]
+ */
+export function getPathByTrees(
+  trees: any[],
+  fn: (data: any) => any,
+  path: any[] = [],
+  results: any[] = []
+) {
+  if (!trees || !trees.length) {
+    return results;
+  }
+
+  for (const tree of trees) {
+    path.push(tree);
+    if (fn(tree)) {
+      results.push([...path]);
+    }
+    if (tree.children) {
+      getPathByTrees(tree.children, fn, path, results);
+    }
+    path.pop();
+  }
+  return results;
+}
